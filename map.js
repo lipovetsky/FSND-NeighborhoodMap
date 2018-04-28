4ca9f27b5019956ebdb93adb6dbdd262f2d3e8ea
Josh Lipovetsky <lipovetskyj9@gmail.com>
2018-04-27 20:40:30 -0400

    Add basic infowindow functionality

--- map.js
+++ map.js
@@ -2,39 +2,53 @@ var map;
 var map_center;
 var path_bounds;
 var addresses;
-var geocoder;
 
 
 addresses = [
-  '1680 E 15th Ave, Eugene, OR 97401',
-  '1430 Johnson Lane, Eugene, OR 97403',
-  '1585 E 13th Ave, Eugene, OR 97403',
-  '1883 University St, Eugene, OR 97403',
-  '70 W 29th Ave, Eugene, OR 97405',
-  '20 E Broadway, Eugene, OR 97401',
-  '100 W 10th Ave, Eugene, OR 97401',
-  '339 E. 11th Ave., Eugene, OR 97401',
-  '433 Willamette St, Eugene, OR 97401',
-  '239 E 14th Ave, Eugene, OR 97401',
-  '1010 Willamette St, Eugene, OR 97401',
-  '62 W Broadway, Eugene, OR 97401',
-  '124 W Broadway, Eugene, OR 97401',
-  '1290 Oak St, Eugene, OR 97401',
-  '296 E 5th Ave #300, Eugene, OR 97401',
-  '1 Eugene Center, Eugene, OR 97401',
-  '4048, 1590 Willamette St, Eugene, OR 97401',
-  '1933 Franklin Blvd, Eugene, OR 97403'
+  {location: '1430 Johnson Lane, Eugene, OR 97403'},
+  {location: '1585 E 13th Ave, Eugene, OR 97403'}
+  // '1883 University St, Eugene, OR 97403',
+  // '20 E Broadway, Eugene, OR 97401',
+  // '124 W Broadway, Eugene, OR 97401',
+  // '62 W Broadway, Eugene, OR 97401',
+  // '100 W 10th Ave, Eugene, OR 97401',
+  // '339 E. 11th Ave., Eugene, OR 97401',
+  // '1010 Willamette St, Eugene, OR 97401',
+  // '1290 Oak St, Eugene, OR 97401',
+  // '1933 Franklin Blvd, Eugene, OR 97403'
 ]
 
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  map_center = new google.maps.LatLng(44.039181, -123.074271);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: map_center,
+    zoom: 14
+  });
+  makeMarkers(addresses);
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(map_center);
+  });
+};
+
 function makeMarkers(addresses) {
+  var geocoder = new google.maps.Geocoder();
+  var infowindow = new google.maps.InfoWindow;
   for (i = 0; i < addresses.length; i++) {
-    geocoder.geocode( {'address' : address[i]}, function(results, status) {
+    geocoder.geocode( {'address' : addresses[i].location}, function(results, status) {
       if (status == 'OK') {
-        map.sestCetner(results[0].geometry.location);
+        map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location
         });
+        marker.addListener('click', function() {
+          infowindow.setOptions( {
+            content: results[0].formatted_address
+          });
+          infowindow.open(map, marker);
+        })
       } else {
         alert('Geocode failed due to: ' + status);
       }
@@ -42,18 +56,8 @@ function makeMarkers(addresses) {
   }
 }
 
-function initMap() {
-  geocoder = new google.maps.Geocoder();
-  map_center = new google.maps.LatLng(44.039181, -123.074271);
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: map_center,
-    zoom: 15
-  });
-  makeMarkers(addresses);
+function setInfoWindows(marker) {
 
-  google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(map_center);
-  });
 };
 
 
