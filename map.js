ccdfdb268de12c214dc2a2c92f26456e422d2e69
Josh Lipovetsky <lipovetskyj9@gmail.com>
2018-06-02 08:32:44 -0400

    Consolidate markers to single click function called addlinks

--- map.js
+++ map.js
@@ -5,20 +5,19 @@ var theTitle;
 
 var model = {
     addresses: [
-    {title: 'Jordan Schnitzer Museum of Art', location: '1430 Johnson Lane, Eugene, OR 97403'},
-    {title: 'University of Oregon', location: '1585 E 13th Ave, Eugene, OR 97403'},
-    {title: 'Gutenberg College', location: '1883 University St, Eugene, OR 97403'},
-    {title: 'Voodoo Doughnut', location: '20 E Broadway, Eugene, OR 97401'},
-    {title: 'The Jazz Station', location: '124 W Broadway, Eugene, OR 97401'},
-    {title: 'Cowfish Dance Club & Cafe', location: '62 W Broadway, Eugene, OR 97401'},
-    {title: 'Eugene Public Library', location: '100 W 10th Ave, Eugene, OR 97401'},
-    {title: 'Cheba Hut', location: '339 E. 11th Ave., Eugene, OR 97401'},
-    {title: 'McDonald Theater', location: '1010 Willamette St, Eugene, OR 97401'},
-    {title: 'Level Up Arcade', location: '1290 Oak St, Eugene, OR 97401'},
-    {title: 'Empire Buffet', location: '1933 Franklin Blvd, Eugene, OR 97403'}
+    {title: "Jordan Schnitzer Museum of Art", location: "1430 Johnson Lane, Eugene, OR 97403"},
+    {title: "University of Oregon", location: "1585 E 13th Ave, Eugene, OR 97403"},
+    {title: "Gutenberg College", location: "1883 University St, Eugene, OR 97403"},
+    {title: "Voodoo Doughnut", location: "20 E Broadway, Eugene, OR 97401"},
+    {title: "The Jazz Station", location: "124 W Broadway, Eugene, OR 97401"},
+    {title: "Cowfish Dance Club & Cafe", location: "62 W Broadway, Eugene, OR 97401"},
+    {title: "Eugene Public Library", location: "100 W 10th Ave, Eugene, OR 97401"},
+    {title: "Cheba Hut", location: "339 E. 11th Ave., Eugene, OR 97401"},
+    {title: "McDonald Theater", location: "1010 Willamette St, Eugene, OR 97401"},
+    {title: "Level Up Arcade", location: "1290 Oak St, Eugene, OR 97401"},
+    {title: "Empire Buffet", location: "1933 Franklin Blvd, Eugene, OR 97403"}
   ],
-  markers: ko.observableArray([]),
-  theAnswer: ko.observableArray([])
+  markers: ko.observableArray([])
 };
 
 
@@ -26,24 +25,24 @@ function initMap() {
   geocoder = new google.maps.Geocoder();
   infowindow = new google.maps.InfoWindow;
   map_center = new google.maps.LatLng(44.039181, -123.074271);
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: map_center,
     zoom: 14
   });
   makeMarkers();
 
-  google.maps.event.addDomListener(window, 'resize', function() {
+  google.maps.event.addDomListener(window, "resize", function() {
     map.setCenter(map_center);
-  });
+  })
 };
 
 
 function makeMarkers() {
   var geocoder = new google.maps.Geocoder();
   for (let  i = 0; i < model.addresses.length; i++) {
-    geocoder.geocode({'address' : model.addresses[i].location}, function(results, status) {
+    geocoder.geocode({"address" : model.addresses[i].location}, function(results, status) {
         theTitle = model.addresses[i].title;
-          if (status == 'OK') {
+          if (status == "OK") {
             map.setCenter(results[0].geometry.location);
               marker = new google.maps.Marker({
               map: map,
@@ -51,70 +50,29 @@ function makeMarkers() {
               position: results[0].geometry.location
             });
             model.markers.push(marker);
-            // console.log(model.markers()[i]);
-            addMessagetoMarker(model.markers()[i], theTitle);
+            marker.addListener("click", (function(markerCopy) {
+              return function() {
+                yoyo.addLinks(markerCopy);
+              }
+            })(marker));
           } else {
-            alert('Geocode failed due to: ' + status);
+            alert("Geocode failed due to: " + status);
           }
       });
-
-      // console.log(theTitle);
-      // google.maps.event.addListener(marker, 'click', (function(titleCopy) {
-      //   return function() {
-      //     console.log(titleCopy);
-      //   }
-      // }(theTitle)));
-      // markers.addListener('click', function() {
-      //   markers.setAnimation(google.maps.Animation.BOUNCE);
-      //   setTimeout(function () {
-      //     marker.setAnimation(null);
-      //   }, 1400);
-      //     infowindow.setOptions( {
-      //       content: theTitle
-      //     });
-      //     infowindow.open(map, marker);
-      //   });
       }
 };
-
-
-
-function addMessagetoMarker(marker, message) {
-  // for (i = 0; i < markers.length; i++) {
-  //   console.log(markers[i]);
-  // }
-  marker.addListener('click', function() {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(function () {
-      marker.setAnimation(null);
-    }, 1400);
-      infowindow.setOptions( {
-        content: message
-      });
-      infowindow.open(map, marker);
-
-    });
-};
-
-function displayMessage(map, marker) {
-  infowindow.open(map, marker)
-}
 
 function hideMarker(marker) {
   marker.setMap(null);
 };
 
-function addLocation(location) {
-  model.theAnswer.push(location);
-  console.log(location);
-}
 
 function viewModel(locations, marker) {
+  query = ko.observable("");
   var self = this;
-  console.log(marker);
-  for (var i=0; i < locations.length; i++) {
-    addLocation(locations[i]);
-  }
+  // for (var i=0; i < locations.length; i++) {
+  //   addLocation(locations[i]);
+  // }
   // marker.forEach(function(element) {
   //   console.log(element);
   //   console.log("YO!");
@@ -123,14 +81,13 @@ function viewModel(locations, marker) {
   for (var i = 0; i < locations.length; i++) {
     name = locations[i].title;
     address = locations[i].location;
-    console.log(model.theAnswer()[i]);
     console.log(marker[i]);
 
     // self.names.push({ title: name, location: address, marker: theMarker });
     self.addLinks = function(num) {
         console.log(num);
         console.log(num.map);
-        num.setAnimation(google.maps.Animation.BOUNCE);
+          num.setAnimation(google.maps.Animation.BOUNCE);
           setTimeout(function () {
             num.setAnimation(null);
           }, 1400);
@@ -138,17 +95,44 @@ function viewModel(locations, marker) {
               content: num.title
             });
             infowindow.open(map, num);
+
+          // });
+        // num.setAnimation(google.maps.Animation.BOUNCE);
+        //   setTimeout(function () {
+        //     num.setAnimation(null);
+        //   }, 1400);
+        //     infowindow.setOptions( {
+        //       content: num.title
+        //     });
+            // infowindow.open(map, num);
         // var theMarker = markers[i]
         // console.log(theMarker);
         // addMessagetoMarker(theMarker, 'love');
-
+        // console.log(ko.utils.arrayFilter(locations.title));
     };
-  };
+  }
+  function search(value) {
+    for (var a in locations) {
+      locations.removeAll();
+      console.log(locations[a]);
+      if(locations[a].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        locations.push(locations[a]);
+      }
+    }
+  }
 
-
+  var theLocations = {
+    query: ko.observable("")
+    }
 };
 
-ko.applyBindings(new viewModel(model.addresses, model.markers()));
+viewModel.locations = ko.dependentObservable(function() {
+  console.log(viewModel);
+}, viewModel)
+
+var yoyo = new viewModel(model.addresses, model.markers())
+ko.applyBindings(yoyo);
+viewModel.theLocations.query.subscribe(viewModel.search);
 
 // Dude. You need one big view model with all the observables. Because it ain't getting bound!!!!
 
@@ -182,4 +166,7 @@ ko.applyBindings(new viewModel(model.addresses, model.markers()));
 // 6. http://todomvc.com/examples/knockoutjs/
 // For getting reference to create a dynamic search box.
 // 7. http://knockoutjs.com/documentation/value-binding.html
+// 8. https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+// 9. http://jsfiddle.net/mythical/XJEzc/
+// For helping with creating a filterable search box.
 // Learning how to create a dynamic search box with valueUpdate: afterkeydown
